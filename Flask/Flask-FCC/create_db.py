41ce6977e2a8db590f27dadc82afa153900dcc7b
Arjun Maniyani <arjunrmaniyani@gmail.com>
2023-10-02 19:33:20 +0530

    uploaded Flask-App

---
+++ Flask/Flask-FCC/create_db.py
@@ -0,0 +1,6 @@
+# Can be done using the python interactive shell
+
+# import db from app.py
+from app import db
+
+db.create_all()
---
+++ README.md
@@ -0,0 +1,1 @@
+# This is just a repository to keep track of everything I learnt and the notes I made.
